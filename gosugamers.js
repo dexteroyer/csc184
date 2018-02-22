652498aba2652542ff71b89560f2da28045dd608
Dexter Esin <sdexterous@yahoo.com>
2018-02-22 22:55:54 +0800

    gosugamers.js

---
+++ gosugamers.js
@@ -0,0 +1,20 @@
+{
+    url: 'http://www.gosugamers.net/dota2/tournaments/7331-hitbox-eu-championship-3/matches/86826-team-alternate-dota-2-vs-myinsanity',
+    home:
+     { name: 'Team Alternate Dota 2',
+       url: '/dota2/teams/12074-team-alternate-dota-2',
+       country: 'Poland',
+       rank: 62,
+       score: 2 },
+    away:
+     { name: 'mYinsanity',
+       url: '/dota2/teams/2820-myinsanity',
+       country: 'Serbia',
+       rank: 84,
+       score: 1 },
+    status: 'Complete',
+    type: 'dota2',
+    rounds: 'Best of 3',
+    valueBet: true,
+    datetime: 1440522000
+}
